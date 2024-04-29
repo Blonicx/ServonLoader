736c470684361ebd7af3f96d34c575123108d7d1
Blonicx <Belte_YT@outlook.com>
2024-04-29 14:04:47 +0200

    Updated the ServonLoader main File

--- src/main/java/blonicx/servonloader/PluginManager.java
+++ src/main/java/blonicx/servonloader/PluginManager.java
@@ -1,2 +1,59 @@
-package blonicx.servonloader;public class PluginManager {
+package blonicx.servonloader;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PluginManager {
+
+    private ServonLoader plugin;
+    public static List<File> loadedPluginFiles = new ArrayList<>();
+
+    public PluginManager(ServonLoader plugin) {
+        this.plugin = plugin;
+        this.loadedPluginFiles = new ArrayList<>();
+    }
+
+    public static void loadPlugins(File pluginFolder) {
+        if (!pluginFolder.exists()) {
+            pluginFolder.mkdirs(); // Create the plugin folder if it doesn't exist
+        }
+
+        File[] files = pluginFolder.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile() && file.getName().endsWith(".jar")) {
+                    try {
+                        Plugin plugin = Bukkit.getServer().getPluginManager().loadPlugin(file);
+                        if (plugin != null) {
+                            Bukkit.getServer().getPluginManager().enablePlugin(plugin);
+                            loadedPluginFiles.add(file);
+                            Bukkit.getLogger().info("Loaded plugin: " + plugin.getName());
+                        }
+                    } catch (Exception e) {
+                        Bukkit.getLogger().warning("Failed to load plugin from file: " + file.getName());
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    public static void unloadPlugins() {
+        for (File file : loadedPluginFiles) {
+            try {
+                Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin(file.getName());
+                if (plugin != null) {
+                    Bukkit.getServer().getPluginManager().disablePlugin(plugin);
+                    Bukkit.getLogger().info("Unloaded plugin: " + plugin.getName());
+                }
+            } catch (Exception e) {
+                Bukkit.getLogger().warning("Failed to unload plugin from file: " + file.getName());
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- src/main/java/blonicx/servonloader/ServonLoader.java
+++ src/main/java/blonicx/servonloader/ServonLoader.java
@@ -1,65 +1,20 @@
 package blonicx.servonloader;
 
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
-public final class ServonLoade extends JavaPlugin {
+public final class ServonLoader extends JavaPlugin {
 
-    private List<File> loadedPluginFiles;
+    public File dataFolder = getDataFolder();
 
     @Override
     public void onEnable() {
-        this.loadedPluginFiles = new ArrayList<>();
-        loadPlugins();
+        PluginManager.loadPlugins(dataFolder);
     }
 
     @Override
     public void onDisable() {
-        unloadPlugins();
-    }
-
-    private void loadPlugins() {
-        File pluginFolder = getDataFolder(); // Get the plugin folder
-        if (!pluginFolder.exists()) {
-            pluginFolder.mkdirs(); // Create the plugin folder if it doesn't exist
-        }
-
-        File[] files = pluginFolder.listFiles();
-        if (files != null) {
-            for (File file : files) {
-                if (file.isFile() && file.getName().endsWith(".jar")) {
-                    try {
-                        Plugin plugin = getServer().getPluginManager().loadPlugin(file);
-                        if (plugin != null) {
-                            getServer().getPluginManager().enablePlugin(plugin);
-                            loadedPluginFiles.add(file);
-                            getLogger().info("Loaded plugin: " + plugin.getName());
-                        }
-                    } catch (Exception e) {
-                        getLogger().warning("Failed to load plugin from file: " + file.getName());
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-    }
-
-    private void unloadPlugins() {
-        for (File file : loadedPluginFiles) {
-            try {
-                Plugin plugin = getServer().getPluginManager().getPlugin(file.getName());
-                if (plugin != null) {
-                    getServer().getPluginManager().disablePlugin(plugin);
-                    getLogger().info("Unloaded plugin: " + plugin.getName());
-                }
-            } catch (Exception e) {
-                getLogger().warning("Failed to unload plugin from file: " + file.getName());
-                e.printStackTrace();
-            }
-        }
+        PluginManager.unloadPlugins();
     }
 }
